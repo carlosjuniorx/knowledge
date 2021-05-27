2fbbcf3f3f549a8ea8d4c6c2f9b7230f9f4d281a
carlos roberto roballo mello junior <www.juniorsolo@hotmail.com>
2021-05-27 19:44:46 -0300

    Component home

---
+++ versao-inicial/frontend/src/components/template/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <footer class="footer">
+        <span>Copyright <strong>Junior</strong>© 2020-2021</span>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style>
+    .footer{
+        grid-area: footer;
+        background-color: #ddd;
+        color: #333;
+        padding-right: 10px;
+
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        padding-right: 10px;
+    }
+</style>
--- versao-inicial/frontend/src/main.js
+++ versao-inicial/frontend/src/main.js
@@ -1,9 +1,21 @@
+import 'font-awesome/css/font-awesome.css'
+
 import Vue from 'vue'
 
 import App from './App'
+import store from './config/store'
+import router from './config/router'
+
+import './config/bootstrap'
 
 Vue.config.productionTip = false
 
+//Temporário
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6OCwibmFtZSI6IkpvYW8iLCJlbWFpbCI6ImpvYW9AaG90bWFpbC5jb20iLCJhZG1pbiI6ZmFsc2UsImlhdCI6MTYyMjEyNDYwOCwiZXhwIjoxNjIyMzgzODA4fQ.LOCB0mGdzF8v_fz7TgDUbQp066IdZCUGIh4e5kvKkLk'
+
+
 new Vue({
+  router,
+  store,
   render: h => h(App)
 }).$mount('#app')
